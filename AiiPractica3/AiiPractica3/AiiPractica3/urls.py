3fda14a5d6fed4a2bcda20523da6dc2d19a94689
Francisco Gago <frangago71@gmail.com>
2025-12-14 23:45:37 +0100

    config inicial

--- AiiPractica3/AiiPractica3/AiiPractica3/urls.py
+++ AiiPractica3/AiiPractica3/AiiPractica3/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for practica3 project.
+URL configuration for AiiPractica3 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
