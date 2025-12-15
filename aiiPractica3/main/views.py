8e6aabb58ce1c38fc24cfa665d4febb3dfb886c5
rafaelroodrgz <rafaelromu24@gmail.com>
2025-12-15 13:15:45 +0100

    commit rafa

--- aiiPractica3/main/views.py
+++ aiiPractica3/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from main.populateDB import populate
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html',{'STATIC_URL':settings.STATIC_URL})
+
+
+def carga(request):
+    if request.method=='POST':
+        if 'Aceptar' in request.POST:      
+            a, b = populate()
+            mensaje="Se han almacenado: " + str(a) +" Animes, " + str(b) +" Puntuaciones"
+            return render(request, 'cargaBD.html', {'mensaje':mensaje})
+        else:
+            return redirect("/")
+           
+    return render(request, 'confirmacion.html')
